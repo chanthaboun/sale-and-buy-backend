92745c73a27dc200d841bcb49af065415710ffc5
chanthaboun <chanthaboun753@gmail.com>
2023-01-23 16:43:23 +0700

    commit1

---
+++ routes/userRoutes.js
@@ -0,0 +1,14 @@
+import express from "express";
+import {
+  loginController,
+  registerController,
+} from "../controllers/userController.js";
+
+const userRouter = express.Router();
+
+userRouter.post("/login", loginController);
+
+userRouter.post("/register", registerController);
+
+
+export default userRouter;
